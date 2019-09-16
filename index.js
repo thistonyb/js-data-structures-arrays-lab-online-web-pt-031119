2591c13b1f008f1685ccb2c5623fd229e8e7640b
Tony Baker <this.tonybaker@gmail.com>
2019-09-16 19:25:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function prependDriver(name) {
 }
 
 function removeLastDriver() {
-  const newDrivers = drivers.pop
+  const newDrivers = drivers.slice(0, drivers.lenght - 1)
   return newDrivers
 }
 
