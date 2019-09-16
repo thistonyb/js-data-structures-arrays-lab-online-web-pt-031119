044287713364cd200256ab1c36f2855c16b3694c
Tony Baker <this.tonybaker@gmail.com>
2019-09-16 18:46:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   drivers.push(name);
 }
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name);
+}
